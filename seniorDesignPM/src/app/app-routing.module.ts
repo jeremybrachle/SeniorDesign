b187d416ff30f191627f5976b3395705b30b154a
Jeremy Brachle <jeremybrachle@gmail.com>
2019-02-01 03:42:43 -0600

    Home Page and Navigation
    Finalized home page and navigation for drop 1 deliverable. So far this is very simple and will be expanded upon in future commits.

--- seniorDesignPM/src/app/app-routing.module.ts
+++ seniorDesignPM/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: '../app/pages/home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: '../app/pages/login/login.module#LoginPageModule' },
 
 ];
 

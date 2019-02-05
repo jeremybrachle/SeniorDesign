697dab67c1ce89cf7c143c25f384ff2eb4ae04ae
Lagombi <cgoehring@smu.edu>
2019-02-05 17:42:28 -0600

    Set up Chase branch, first Booking commit

--- seniorDesignPM/src/app/pages/booking/booking.module.ts
+++ seniorDesignPM/src/app/pages/booking/booking.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { BookingPage } from './booking.page';
+import { HeadingComponent } from '../../heading/heading.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [BookingPage]
+  declarations: [BookingPage, HeadingComponent]
 })
 export class BookingPageModule {}

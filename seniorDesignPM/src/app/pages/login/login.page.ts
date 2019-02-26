a0735f0f20b8298b87c154dce6c2e9562e3344ec
Nora Potenti <npotenti@smu.edu>
2019-02-26 16:13:57 -0600

    Static login page created

--- seniorDesignPM/src/app/pages/login/login.page.ts
+++ seniorDesignPM/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ForgotPasswordModalPage } from "../Modals/forgot-password-modal/forgot-password-modal.page"
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  async presentResetPWModal(){
+    const modal = await this.modalController.create({
+      component: ForgotPasswordModalPage
+    });
+
+    await modal.present();
   }
 
 }

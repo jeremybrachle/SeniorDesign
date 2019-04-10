40c8f407b77422513a8c9c0354abb004cd18c616
Jeremy Brachle <jeremybrachle@gmail.com>
2019-04-10 02:20:42 -0500

    Fixed up the experiences ts file

--- seniorDesignPM/src/app/pages/entertainment/entertainment.page.ts
+++ seniorDesignPM/src/app/pages/entertainment/entertainment.page.ts
@@ -13,20 +13,28 @@ import { EntertainmentItem } from './../../models/entertainment-item-model';
   styleUrls: ['./entertainment.page.scss'],
 })
 export class EntertainmentPage implements OnInit {
+  // variables for the number of options selected for each type of add on
   numDining: any = 0;
   numSportsConcerts: any = 0;
+  numAccomodations: any = 0;
+  numNightLife: any = 0;
   numExperiences: any = 0;
 
   // entertainment option arrays
-  // dining options
   allDiningOptions = new Array;
+  allSportsConcertsOptions = new Array;
+  allAccomodationsOptions = new Array;
+  allNightLifeOptions = new Array;
+  allExperiencesOptions = new Array;
 
+  // constructor (make a modal controller)
   constructor(public modalController: ModalController) { }
 
+  // on initialization, get the data
   ngOnInit() {
     // get all the entertainment options here from the database
 
-    // dining options
+    // 1. dining options
     let diningOpt1 = new EntertainmentItem(
       'Ristorante Lorem',
       80.00,
@@ -43,15 +51,53 @@ export class EntertainmentPage implements OnInit {
     );
     this.allDiningOptions.push(diningOpt1);
     this.allDiningOptions.push(diningOpt2);
-    // this.allDiningOptions.indexOf(diningOpt1);
-    // diningOpt1.selectOption();
-    // this.allDiningOptions.length;
+
+    // 2. sports options
+    let sportsConcertsOpt1 = new EntertainmentItem(
+      'Cowboys Game',
+      1000.00,
+      'sportsConcerts',
+      'https://images-na.ssl-images-amazon.com/images/I/41HYP2h9tML._SL500_AC_SS350_.jpg',
+      false
+    );
+    this.allSportsConcertsOptions.push(sportsConcertsOpt1);
+
+    // 3. accomodations options
+    let accomodationsOpt1 = new EntertainmentItem(
+      'yeet',
+      0.00,
+      'accomodations',
+      'yeet',
+      false
+    );
+    this.allAccomodationsOptions.push(accomodationsOpt1);
+
+    // 4. night life options
+    let nightLifeOpt1 = new EntertainmentItem(
+      'Sample Night Life',
+      340.00,
+      'nightLife',
+      'https://media.timeout.com/images/100898801/630/472/image.jpg',
+      false
+    );
+    this.allNightLifeOptions.push(nightLifeOpt1);
+
+    // 5. experiences toptions
+    let experiencesOpt1 = new EntertainmentItem(
+      'Sample Helicopter Ride',
+      1000.00,
+      'experiences',
+      'https://epichelicopters.com/assets/site_images/_380x215_crop_center-center_100/dallas-fort-worth-aerial-video_0.jpg',
+      false
+    );
+    this.allExperiencesOptions.push(experiencesOpt1);
+
 
   }
 
   // code for modals:
 
-  // dining modal
+  // 1. dining modal
   async presentDiningModal() {
     const modal = await this.modalController.create({
       component: DiningModalPage,
@@ -62,80 +108,87 @@ export class EntertainmentPage implements OnInit {
     // present the modal
     await modal.present();
 
-    // await modal.onWillDismiss();
+    // check the response for what options are picked (if any)
+    const {data} = await modal.onWillDismiss();
+
+    // set the number of dining options selected
+    this.numDining = data.numSelected;
+  }
+
+  // 2. concerts modal
+  async presentSportsConcertsModal() {
+    const modal = await this.modalController.create({
+      component: SportsConcertsModalPage,
+      // send in the sports/concerts options array
+      componentProps: {sportConcertsArr: this.allSportsConcertsOptions}
+    });
+
+    // present the modal
+    await modal.present();
 
     // check the response for what options are picked (if any)
     const {data} = await modal.onWillDismiss();
 
-    // console.log(data);
-    this.numDining = data.numSelected;
-  }
-
-
-
-
-
-
-
-
-
-
-
-  // concerts modal
-  async presentSportsConcertsModal() {
-    const modal = await this.modalController.create({
-      component: SportsConcertsModalPage
-    });
-
-    await modal.present();
-
-    const {data} = await modal.onWillDismiss();
-    console.log(data);
+    // set the number of sports/concerts options selected
     this.numSportsConcerts = data.numSelected;
   }
 
-  async presentExperiencesModal() {
-    const modal = await this.modalController.create({
-      component: ExperiencesModalPage
-    });
-
-    await modal.present();
-
-    const {data} = await modal.onWillDismiss();
-    console.log(data);
-    this.numExperiences = data.numSelected;
-  }
-
+  // 4. night life modal
   async presentNightLifeModal() {
     const modal = await this.modalController.create({
-      component: NightLifeModalPage
+      component: NightLifeModalPage,
+      // send in the night life options array
+      componentProps: {nightLifeArr: this.allNightLifeOptions}
     });
 
+    // present the modal
     await modal.present();
 
+    // check the response for what options are picked (if any)
     const {data} = await modal.onWillDismiss();
-    console.log(data);
-    this.numExperiences = data.numSelected;
+
+    // set the number of night life options selected
+    this.numNightLife = data.numSelected;
   }
 
+  // 5. experiences modal
+  async presentExperiencesModal() {
+    const modal = await this.modalController.create({
+      component: ExperiencesModalPage,
+      // send in the experiences options array
+      componentProps: {experiencesArr: this.allExperiencesOptions}
+    });
+
+    // present the modal
+    await modal.present();
+
+    // check the response for what options are picked (if any)
+    const {data} = await modal.onWillDismiss();
+
+    // set the number of dining options selected
+    this.numExperiences = data.numExperiences;
+  }
+
+  // reset the values of the variables for selected options
   cancel() {
     this.numDining = 0;
     this.numSportsConcerts = 0;
+    this.numAccomodations = 0;
+    this.numNightLife = 0;
     this.numExperiences = 0;
   }
 
+  // cancel confirm modal
   async presentCancelModal() {
     const modal = await this.modalController.create({
       component: ConfirmCancelPage,
       cssClass: 'cancel-modal'
     });
-
+    // show the modal and dismiss it
     await modal.present();
-
     await modal.onWillDismiss();
-
+    // reset the variables
     this.cancel();
-
   }
 
 }
